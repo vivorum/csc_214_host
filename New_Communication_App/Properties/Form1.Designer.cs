1eb47cb88eb3d8a5d7c9aee57f61859bffe474c9
kingdomdude8 <mluciano98@gmail.com>
2021-05-06 18:30:01 -0400

    Added Professor's Code

--- New_Communication_App/Properties/Form1.Designer.cs
+++ New_Communication_App/Properties/Form1.Designer.cs
@@ -41,6 +41,9 @@ namespace New_Communication_App.Properties
             this.label2 = new System.Windows.Forms.Label();
             this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
+            this.serPortList = new System.Windows.Forms.ComboBox();
+            this.doConnect = new System.Windows.Forms.Button();
+            this.statusTB = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
@@ -112,7 +115,7 @@ namespace New_Communication_App.Properties
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(217, 6);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 6);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(400, 400);
             this.pictureBox1.TabIndex = 6;
@@ -164,11 +167,42 @@ namespace New_Communication_App.Properties
             this.label3.Text = "Temp";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
+            // serPortList
+            // 
+            this.serPortList.FormattingEnabled = true;
+            this.serPortList.Location = new System.Drawing.Point(425, 420);
+            this.serPortList.Name = "serPortList";
+            this.serPortList.Size = new System.Drawing.Size(82, 21);
+            this.serPortList.TabIndex = 12;
+            this.serPortList.SelectedIndexChanged += new System.EventHandler(this.serPortList_SelectedIndexChanged);
+            // 
+            // doConnect
+            // 
+            this.doConnect.BackColor = System.Drawing.Color.Lime;
+            this.doConnect.Location = new System.Drawing.Point(375, 418);
+            this.doConnect.Name = "doConnect";
+            this.doConnect.Size = new System.Drawing.Size(44, 23);
+            this.doConnect.TabIndex = 13;
+            this.doConnect.Text = "CNCT";
+            this.doConnect.UseVisualStyleBackColor = false;
+            this.doConnect.Click += new System.EventHandler(this.doConnect_Click);
+            // 
+            // statusTB
+            // 
+            this.statusTB.Location = new System.Drawing.Point(513, 12);
+            this.statusTB.Multiline = true;
+            this.statusTB.Name = "statusTB";
+            this.statusTB.Size = new System.Drawing.Size(275, 403);
+            this.statusTB.TabIndex = 14;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusTB);
+            this.Controls.Add(this.doConnect);
+            this.Controls.Add(this.serPortList);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.numericUpDown2);
             this.Controls.Add(this.label2);
@@ -206,5 +240,8 @@ namespace New_Communication_App.Properties
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.NumericUpDown numericUpDown2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox serPortList;
+        private System.Windows.Forms.Button doConnect;
+        private System.Windows.Forms.TextBox statusTB;
     }
 }
--- New_Communication_App/Properties/Form1.cs
+++ New_Communication_App/Properties/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,10 +18,31 @@ namespace New_Communication_App.Properties
         private int picType = 0;
         private string fileName = "";
         private int imageNum = 0;
-        private int temp = 10; 
+        private int temp = 10;
+        static SerialPort serialPort = null;
+
+      int xframe;
+      int yframe;
+      float xofs;
+      float yofs;
+      float xsize;
+      float ysize;
+      float xbin;
+      float ybin;
+      float xsec;
+      float xmsec;
+        int blen;
+
+
         public Form1()
         {
             InitializeComponent();
+
+            string[] coms = SerialPort.GetPortNames();
+            serPortList.DataSource = coms;
+            int portSelect = -1;
+
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -135,6 +157,64 @@ namespace New_Communication_App.Properties
             {
                 temp = Convert.ToInt32(Math.Round(numericUpDown2.Value, 0));
             }
+        }
+
+        private void serPortList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void doConnect_Click(object sender, EventArgs e)
+        {
+            string port = (string)serPortList.SelectedItem;
+            serialPort = new SerialPort(port) { BaudRate = 115200 };
+            try { serialPort.Open(); }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to open serial port");
+                return;
+            }
+            //Once serial port is open, lets get some info
+
+            xframe = getParam("xframe?"); // get the dimensions of sensor 
+            statusTB.AppendText("xframe --> " + xframe + Environment.NewLine);
+            yframe = getParam("yframe?");
+            statusTB.AppendText("yframe --> " + yframe + Environment.NewLine);
+            xofs = getParam("xoffset?"); // get x,y,w,h of Region of Interest (ROI) 
+            statusTB.AppendText("xoffset --> " + xofs + Environment.NewLine);
+            yofs = getParam("yoffset?");
+            statusTB.AppendText("yoffset --> " + yofs + Environment.NewLine);
+            xsize = getParam("xsize?");
+            statusTB.AppendText("xsize --> " + xsize + Environment.NewLine);
+            ysize = getParam("ysize?");
+            statusTB.AppendText("ysize --> " + ysize + Environment.NewLine);
+            xbin = getParam("xbin?"); // get current bin 
+            statusTB.AppendText("xbin --> " + xbin + Environment.NewLine);
+            ybin = getParam("ybin?");
+            statusTB.AppendText("ybin --> " + ybin + Environment.NewLine);
+            xsec = getParam("xsec?"); // get exposure in seconds 
+            statusTB.AppendText("xsec --> " + xsec + Environment.NewLine);
+            xmsec = getParam("xmsec?");
+            statusTB.AppendText("xmsec --> " + xmsec + Environment.NewLine);
+
+            blen = 2 * xframe * yframe;
+
+            Console.WriteLine("blen --> " + blen);
+
+        }
+
+        bool setParam(string cmd, int val)
+        {
+            serialPort.Write(string.Format("{0} {1}\n", cmd, val));
+            string xf = serialPort.ReadLine().Trim();
+            return xf.StartsWith("OK");
+        }
+
+        int getParam(string cmd) // retrieves a integer parameter 
+        {
+            serialPort.Write(cmd + "\n");
+            string xf = serialPort.ReadLine().Trim();
+            string[] ary = xf.Split(' ');
+            return Convert.ToInt32(ary[0]);
         }
     }
 }
